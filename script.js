bdf330438c9281664d3ee6c6e5280aa11173ec3e
Matthew <matt-hope@hotmail.com>
2020-09-17 15:52:45 -0400

    added face detection js files

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 
+const MODEL_URL = '/models';
+
+
 const moodApp = {};
 
+moodApp.init = () => {
+    console.log("innit...");
 
+    const imageUpload;
+    $(".image-upload-form").on('submit', () => {
+
+        imageUpload = $('#image-upload');
+    })
+}
+
+//doc ready
+
+$(() => {
+  await faceapi.loadSsdMobilenetv1Model(MODEL_URL)
+  await faceapi.loadFaceLandmarkModel(MODEL_URL)
+  await faceapi.loadFaceRecognitionModel(MODEL_URL)
+  moodApp.init();
+})
